5fe53309e5f27b037cf4c8d2de523c360f30ef9c
Povilas Kanapickas <povilas@radix.lt>
2021-07-10 14:03:04 +0300

    Setup project version info

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,14 @@
 
 #include "main_window.h"
+#include "version.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+
+    QCoreApplication::setApplicationName("sanescan");
+    QCoreApplication::setApplicationVersion(SANESCAN_VERSION);
 
     sanescan::MainWindow main_window;
     main_window.show();
