a072461b02a687b0727bb7454c4bc8e3e2e70e2a
Povilas Kanapickas <povilas@radix.lt>
2022-05-10 13:56:05 +0300

    ocr: Add header with forward declarations for classes and structs

---
+++ src/ocr/fwd.h
@@ -0,0 +1,40 @@
+/*  SPDX-License-Identifier: GPL-3.0-or-later
+
+    Copyright (C) 2021  Povilas Kanapickas <povilas@radix.lt>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef SANESCAN_LIB_OCR_FWD_H
+#define SANESCAN_LIB_OCR_FWD_H
+
+namespace sanescan {
+
+struct AffineMatrix;
+class HocrException;
+struct OcrBaseline;
+struct OcrBox;
+struct OcrLine;
+struct OcrOptions;
+struct OcrParagraph;
+struct OcrResults;
+struct OcrWord;
+class PdfCanvas;
+class PdfWriter;
+class TesseractRecognizer;
+class TesseractRenderer;
+
+} // namespace sanescan
+
+#endif // SANESCAN_LIB_OCR_FWD_H
